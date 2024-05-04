31d7c963605a43976863fcda62b3fc9c5eca3731
Jeex8 <158838813+Jeex8@users.noreply.github.com>
2024-05-04 18:26:35 +0300

    Delete pythondiscord directory

--- pythondiscord/afmqpqpamfakf.py
+++
@@ -1,8 +0,0 @@
-from dadata import Dadata
-
-
-token = "1c43107faa2f7ad485f777c924c9c0d38459e62e"
-secret = "e23b47eb7e0ce34d86bd81d46ebd780f2cf0a02f"
-dadata = Dadata(token, secret)
-result = dadata.clean("address", "Орск")
-print(result)
